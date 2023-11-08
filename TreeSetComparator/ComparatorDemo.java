d448c17dccd1b22c6f9b27bff525e96b2d203e63
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2023-11-08 07:22:00 +0530

    Create ComparatorDemo.java

---
+++ TreeSetComparator/ComparatorDemo.java
@@ -0,0 +1,23 @@
+
+import java.util.Comparator;
+
+public class ComparatorDemo implements Comparator {
+
+	@Override
+	public int compare(Object o1, Object o2) {
+		Integer d1=(Integer)o1;
+		Integer d2=(Integer)o2;
+		if(d1>d2)
+			return -1;
+		else if(d1<d2)
+			return 1;
+		else
+			return 0;
+	}
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
