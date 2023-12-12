1cfa7ffd7efc91bf2698896b83ef9833968020e1
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2023-12-12 19:15:38 +0530

    Update Cursorlistitr.java

--- Cursorlistitr.java
+++ Cursorlistitr.java
@@ -1,29 +1,36 @@
 import java.util.ArrayList;
 import java.util.ListIterator;
+
 public class Cursorlistitr {
 
-	public static void main(String[] args) {
-		ArrayList<String> data=new ArrayList<>();
-		data.add("Hey");
-		data.add("Hello");
-		data.add("Every");
-		data.add("One");
-		System.out.println(data);
-		ListIterator li=data.listIterator();
-		while(li.hasNext()) {
-			String item=(String)li.next();
-			if(item.equals("This")) {
-				li.add("is a");
-			}
-			if(item.equals("Simple")) {
-				li.remove();
-			}
-			if(item.equals("Java")) {
-				li.set("Program");
-			}
-		}
-		System.out.println(data);
+    public static void main(String[] args) {
+        ArrayList<String> data = new ArrayList<>();
+        data.add("Hey");
+        data.add("Hello");
+        data.add("Every");
+        data.add("One");
+        System.out.println(data);
+        ListIterator li = data.listIterator();
+        while (li.hasNext()) {
+            String item = (String) li.next();
+            if (item.equals("Hey")) {
+                li.set("This");
+            }
+            if (item.equals("Hello")) {
+                li.set("is a");
+            }
+            if (item.equals("Every")) {
+                li.remove();
+            }
+            if (item.equals("Hello")) {
+                li.add("Java Collections");
+            }
+            if (item.equals("One")) {
+                li.set("Program");
+            }
+        }
+        System.out.println(data);
 
-	}
+    }
 
 }
