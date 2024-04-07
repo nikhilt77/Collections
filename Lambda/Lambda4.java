901ced030984be872ffc4aa57e1b8ae1959927b4
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2024-04-08 00:16:23 +0530

    Update Lambda4.java

--- Lambda/Lambda4.java
+++ Lambda/Lambda4.java
@@ -1,11 +1,24 @@
 package com.example.lambda;
-interface Function{
-	public int Calc(String s);
+
+interface StringFunction {
+    public String apply(String s);
 }
+
+interface IntFunction {
+    public int apply(String s);
+}
+
 public class Lambda4 {
 
-	public static void main(String[] args) {
-		Function f=(s)->s.length();
-		System.out.println("Length of String:"+(f.Calc("Hello")));
-	}
+    public static void main(String[] args) {
+        IntFunction lengthFunction = s -> s.length();
+        StringFunction uppercaseFunction = s -> s.toUpperCase();
+        StringFunction reverseFunction = s -> new StringBuilder(s).reverse().toString();
+
+        String input = "Hello";
+
+        System.out.println("Length of String: " + lengthFunction.apply(input));
+        System.out.println("Uppercase String: " + uppercaseFunction.apply(input));
+        System.out.println("Reversed String: " + reverseFunction.apply(input));
+    }
 }
