fb999f80192f715d671c4c951f9eeacabbb09b69
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2024-04-07 15:41:38 +0530

    This is just an introduction to lambda and is not a runnable program

---
+++ LambdaIntro.java
@@ -0,0 +1,19 @@
+//Type1
+public void myName(){
+  sysout("John");
+}
+public void(){
+  sysot("John");
+}
+() -> sysot("John");
+(a,b)->  sysout(a+b);//Type2
+
+//Type3
+public int length(String s){
+  return s.length();
+}
+length(String s){
+  return s.length();
+}
+(s)->return s.length();
+(s)-> s.length();//final expression
