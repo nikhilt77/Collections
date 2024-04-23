dfaf8f559d63012ced742114dced0863772b5f03
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2024-04-23 23:52:39 +0530

    Create Employee.java

---
+++ ParallelStreams/Employee.java
@@ -0,0 +1,22 @@
+package com.example.lambda;
+
+public class Employee {
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	private int salary;
+	public Employee(String name,int salary) {
+		this.name=name;
+		this.salary=salary;
+	}
+}
