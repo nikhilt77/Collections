7234fa16ca8fdc5f5dd2503d75fc87a479a93df7
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2024-07-23 19:39:26 +0530

    Create example.java

---
+++ Generics1/example.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class example {
+
+    static <T> void arg(T V[]) {
+        System.out.println("Length of array is: " + V.length);
+        for (T x : V) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        Integer arr1[] = { 1, 2, 3, 4, 5 };
+        Double arr2[] = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+        Character arr3[] = { 'H', 'E', 'L', 'L', 'O' };
+        arg(arr1);
+        arg(arr2);
+        arg(arr3);
+    }
+}
