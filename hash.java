5414b4c7ac923deadc779f3b8e0c9d3d547debf6
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2024-09-29 01:59:46 +0530

    Create hash.java

---
+++ hash.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class hash {
+
+    public static void main(String args[]) {
+        Scanner P = new Scanner(System.in);
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+        System.out.println("Enter array size:");
+        int n = P.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = P.nextInt();
+            mpp.put(arr[i], mpp.getOrDefault(arr[i], 0) + 1);
+        }
+        Map.Entry<Integer, Integer> max = mpp
+            .entrySet()
+            .stream()
+            .max(Map.Entry.comparingByValue())
+            .orElse(null);
+        System.out.println("Highest frequency element:" + max);
+        if (max != null) {
+            System.out.println("Highest Freq element:" + max.getKey());
+            System.out.println("Frequency:" + max.getValue());
+        }
+    }
+}
